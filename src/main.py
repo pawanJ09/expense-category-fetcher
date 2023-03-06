fe02a785ecd7144285416107db6ff7cdb8b033a6
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-03-06 14:41:58 -0600

    Adding test API Gateway event for test workflow

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+import json
 
 
 def lambda_handler(event, context):
-    print('Inside Lambda Handler')
+    try:
+        event_body = json.loads(event['Records'][0]['body'])
+        print(f'Incoming API Gateway Message: {event_body}')
+        event_path = json.loads(event['Records'][0]['path'])
+        event_http_method = json.loads(event['Records'][0]['httpMethod'])
+    except Exception as e:
+        msg = '\nProcessing error. Check Cloudwatch logs.'
+        raise Exception(msg)
 
 
 if __name__ == '__main__':
