5cc2a11528a431b95efa8fd08e3b4c0729b33707
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-03-06 14:49:42 -0600

    Correcting main.py with reading API Gateway event

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import json
+import os
 
 
 def lambda_handler(event, context):
     try:
-        event_body = json.loads(event['Records'][0]['body'])
+        event_body = json.loads(event['body'])
         print(f'Incoming API Gateway Message: {event_body}')
-        event_path = json.loads(event['Records'][0]['path'])
-        event_http_method = json.loads(event['Records'][0]['httpMethod'])
+        event_path = json.loads(event['path'])
+        event_http_method = json.loads(event['httpMethod'])
     except Exception as e:
         msg = '\nProcessing error. Check Cloudwatch logs.'
         raise Exception(msg)
 
 
 if __name__ == '__main__':
-    lambda_handler(None, None)
+    script_dir = os.path.dirname(__file__)
+    rel_path = '../events/test-agw-event.json'
+    abs_file_path = os.path.join(script_dir, rel_path)
+    with open(abs_file_path) as f:
+        test_event = json.load(f)
+        lambda_handler(test_event, None)
